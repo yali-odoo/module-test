f7012da9e1e7aa5fb9d18529ccc3ea1275225f77
yali-odoo <yali@odoo.com>
2022-06-13 19:58:55 -0400

    add __manifest__.py and __init__.py

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+{
+    'name':'Odoo Academy',
+    
+    'summary':"""Academy app to manage Training""",
+    
+    'description':"""
+        Academy Module to manage Training:
+        -Courses
+        -Sessions
+        -Attendees
+    """,
+    
+    'author':'Odoo',
+    
+    'websie':'https//www.odoo.com',
+    
+    'category':'Training',
+    
+    'version':'0.1',
+    
+    'depends':['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo':[
+        
+        
+    ],
+   
+}
+ 
